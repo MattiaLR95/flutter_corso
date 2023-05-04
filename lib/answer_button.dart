391817f4900371ffd4815e68ca76f5e7de16b931
MattiaLR95 <laroccamattia@virgilio.it>
2023-05-04 12:13:01 +0200

    Creazione pagina di domande con inserimento delle risposte tramite mappatura

---
+++ lib/answer_button.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget {
+  const AnswerButton ({
+    super.key,
+    required this.answerText,
+    required this.onTap,
+  });
+
+  final String answerText;
+  final void Function() onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onTap,
+      style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.symmetric(
+          vertical: 10,
+          horizontal: 40,
+          ),
+        backgroundColor: const Color.fromARGB(197, 10, 0, 205),
+        foregroundColor: const Color.fromRGBO(255, 255, 255, 1),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(40),
+        ),
+      ),
+      child: Text(answerText),
+    );
+  }
+}
--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -1,6 +1,6 @@
-
-
 import 'package:flutter/material.dart';
+import 'package:verifica_moduli_1_2/answer_button.dart';
+import 'package:verifica_moduli_1_2/data/questions.dart';
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
@@ -11,10 +11,28 @@ class QuestionScreen extends StatefulWidget {
   }
 }
 
-class _QuestionScreen extends State<QuestionScreen>{
+class _QuestionScreen extends State<QuestionScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text('QuestionScreen');
-  }
+    final currentQuestion = questions[0];
 
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            currentQuestion.text,
+            style: const TextStyle(
+              color: Color.fromRGBO(255, 255, 255, 1),
+            ),
+          ),
+          const SizedBox(height: 30),
+          ...currentQuestion.answers.map((answer) {
+            return AnswerButton(answerText: answer, onTap: () {});
+          }),
+        ],
+      ),
+    );
+  }
 }
