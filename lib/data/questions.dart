b5065d003ef1e539eedcd913f4777de9c28667ed
MattiaLR95 <laroccamattia@virgilio.it>
2023-05-04 11:00:33 +0200

    Creazione del Model e creazione delle domande

---
+++ lib/data/questions.dart
@@ -0,0 +1,51 @@
+
+
+import 'package:verifica_moduli_1_2/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion(
+    'What are the main building blocks of Flutter UIs?',
+    [
+    'Widgets',
+    'Components',
+    'Blocks',
+    'Functions',
+    ],
+  ),
+  QuizQuestion(
+    'How are Flutter UIs built?',
+    [
+    'By combining widgets in code',
+    'By combyning widgets in a visual editor',
+    'By degining widgets in config files',
+    'By ysing XCode for iOs and Android Studio for Android',
+    ],
+  ),
+  QuizQuestion(
+    'What\'s the purpose of a StatefulWidget?',
+    [
+    'Update UI as data changes',
+    'Update data as UT changes',
+    'Ignore data changes',
+    'Render UI that does not depend on data',
+    ],
+  ),
+  QuizQuestion(
+    'What happens if you change data in a StatelessWidget?',
+    [
+      'The UI is not updated',
+      'The UI is updated',
+      'The closest StatefulWidget is updated',
+      'Any nested StatefulWidgets are updated',
+    ],
+  ),
+  QuizQuestion(
+    'How should you update data inside of StatefulWidgets?',
+    [
+      'By calling setState()',
+      'By calling updateData()',
+      'By calling updateUI()',
+      'By calling updateState()',
+    ],
+  ),
+];
