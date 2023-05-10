0d29debe0b8389cff48e21ead1e35eb27c9efd3c
MattiaLR95 <laroccamattia@virgilio.it>
2023-05-10 12:28:15 +0200

    Rifinimento grafico della pagina risultati

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class QuestionSummary extends StatelessWidget {
   const QuestionSummary(this.summaryData, {super.key});
@@ -14,16 +15,50 @@ class QuestionSummary extends StatelessWidget {
           children: summaryData.map((data) {
             return Row(
               children: [
-                Text(((data['question_index'] as int) + 1).toString()),
+                Container(
+                  width: 30,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.white,
+                  ),
+                  child: Text(
+                    (((data['question_index'] as int) + 1).toString()),
+                    style: GoogleFonts.lato(
+                      color: const Color.fromARGB(183, 0, 0, 0),
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
                 Expanded(
                   child: Column(
                     children: [
-                      Text(data['question'] as String),
+                      Text(
+                        data['question'] as String,
+                        style: GoogleFonts.lato(
+                          color: const Color.fromARGB(183, 0, 0, 0),
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
                       const SizedBox(
                         height: 5,
                       ),
-                      Text(data['user_answers'] as String),
-                      Text(data['correct_question'] as String),
+                      Text(
+                        data['user_answers'] as String,
+                        style: GoogleFonts.lato(
+                          color: const Color.fromARGB(183, 0, 0, 0),
+                          fontSize: 14,
+                        ),
+                      ),
+                      Text(
+                        data['correct_question'] as String,
+                        style: GoogleFonts.lato(
+                          color: const Color.fromARGB(183, 0, 0, 0),
+                          fontSize: 14,
+                        ),
+                      ),
                     ],
                   ),
                 ),
